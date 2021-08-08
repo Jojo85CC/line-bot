17aa93497370ff0fd0b6befe96ee2d3b4a561b7a
Jojo Lin <jkggy3618@gmail.com>
2021-08-08 21:22:28 +0800

    update

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def handle_message(event):
         r ='嗨'
     elif msg =='你吃飯了嗎':
         r ='還沒'
-    elif msg =="你是誰":
+    elif msg =='你是誰':
         r = '我是機器人'
     elif '訂位' in msg:
         r = '你想訂位,是嘛?'
